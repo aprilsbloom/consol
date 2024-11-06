01ef5e1f29832ab1beb7c3bed3004bb9b456a273
paintingofblue <90877067+aprilsbloom@users.noreply.github.com>
2024-11-06 22:16:19 +1300

    move formatting into different functions

--- src/index.ts
+++ src/index.ts
@@ -13,10 +13,29 @@ export class Logger extends OptionsManager {
 		return strftime(format, date);
 	}
 
+	private formatBase(message: string, level: LogLevel): string {
+		return this.options.formats.log
+			.replaceAll('{date}', this.fetchDate())
+			.replaceAll('{altDate}', this.fetchDate(this.options.formats.altDate))
+			.replaceAll('{level}', this.options.strings[LogLevel[level].toLowerCase() as LogType]!)
+			.replaceAll('{message}', message);
+	}
+
+	private formatColors(message: string, level: LogLevel): string {
+		return message
+			.replaceAll('{colors.level}', this.options.colors.ansi[LogLevel[level].toLowerCase() as LogType]!)
+			.replaceAll('{colors.info}', this.options.colors.ansi.info!)
+			.replaceAll('{colors.success}', this.options.colors.ansi.success!)
+			.replaceAll('{colors.warning}', this.options.colors.ansi.warning!)
+			.replaceAll('{colors.error}', this.options.colors.ansi.error!)
+			.replaceAll('{colors.fatal}', this.options.colors.ansi.fatal!)
+			.replaceAll('{colors.debug}', this.options.colors.ansi.debug!)
+			.replaceAll('{colors.reset}', this.options.colors.ansi.reset);
+	}
+
 	private log(level: LogLevel, message: string, ...args: any[]) {
 		if (level > this.options.logLevel) return;
 
-		const levelLowercase = LogLevel[level].toLowerCase() as LogType;
 		const messageStr = args.length ?
 			// biome-ignore lint/style/useTemplate: template literals make the code harder to read here lol
 		  message + " " + args.map(item => {
@@ -27,23 +46,8 @@ export class Logger extends OptionsManager {
 			}).join(' ') :
 			message;
 
-		// add date, log level & message
-		let fmtdMessage = this.options.formats.log
-			.replaceAll('{date}', this.fetchDate())
-			.replaceAll('{altDate}', this.fetchDate(this.options.formats.altDate))
-			.replaceAll('{level}', this.options.strings[levelLowercase]!)
-			.replaceAll('{message}', messageStr);
-
-		// add colors
-		fmtdMessage = fmtdMessage
-			.replaceAll('{colors.level}', this.options.colors.ansi[levelLowercase]!)
-			.replaceAll('{colors.info}', this.options.colors.ansi.info!)
-			.replaceAll('{colors.success}', this.options.colors.ansi.success!)
-			.replaceAll('{colors.warning}', this.options.colors.ansi.warning!)
-			.replaceAll('{colors.error}', this.options.colors.ansi.error!)
-			.replaceAll('{colors.fatal}', this.options.colors.ansi.fatal!)
-			.replaceAll('{colors.debug}', this.options.colors.ansi.debug!)
-			.replaceAll('{colors.reset}', this.options.colors.ansi.reset);
+		let fmtdMessage = this.formatBase(messageStr, level); // add date, log level & message
+		fmtdMessage = this.formatColors(fmtdMessage, level); // add colors
 
 		console.log(fmtdMessage);
 	}
