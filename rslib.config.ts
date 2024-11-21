67ac3f41dae0254a8bd6fe0436fa46005045688d
jane <90877067+aprilsbloom@users.noreply.github.com>
2024-11-21 17:31:02 +1300

    add release workflow (#1)

--- rslib.config.ts
+++ rslib.config.ts
@@ -1,31 +1,50 @@
 import { defineConfig, type LibConfig } from '@rslib/core';
 
+const bundle = true;
 const sharedLib: LibConfig = {
 	syntax: 'es2024',
+	bundle,
 	dts: {
-		bundle: true,
+		bundle,
+		autoExtension: true,
+	},
+	output: {
+		distPath: {
+			root: './dist',
+		},
+		minify: {
+			js: true,
+			jsOptions: {
+				minimizerOptions: {
+					compress: true,
+					mangle: true,
+					minify: true,
+				}
+			}
+		}
 	},
 };
 
 export default defineConfig({
+	source: {
+    entry: {
+      index: './src/index.ts',
+    },
+		transformImport: [
+      {
+        libraryName: 'lodash',
+        customName: 'lodash/{{ member }}',
+      },
+    ],
+  },
 	lib: [
 		{
 			...sharedLib,
 			format: 'esm',
-			output: {
-				distPath: {
-					root: './dist/esm',
-				},
-			},
 		},
 		{
 			...sharedLib,
 			format: 'cjs',
-			output: {
-				distPath: {
-					root: './dist/cjs',
-				},
-			},
 		},
 	]
 });
