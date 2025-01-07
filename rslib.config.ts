1d10daa25f17d5c6b180290dbf31147efbaec7fc
paintingofblue <90877067+aprilsbloom@users.noreply.github.com>
2025-01-07 20:24:07 +1300

    add dev builds, bump deps

--- rslib.config.ts
+++ rslib.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig, type LibConfig } from '@rslib/core';
 
+const prod = process.env.NODE_ENV === 'production';
 const bundle = true;
+const basePath = './dist';
 const sharedLib: LibConfig = {
 	syntax: 'es2024',
 	bundle,
@@ -9,42 +11,49 @@ const sharedLib: LibConfig = {
 		autoExtension: true,
 	},
 	output: {
-		distPath: {
-			root: './dist',
-		},
+
 		minify: {
-			js: true,
+			js: prod,
 			jsOptions: {
 				minimizerOptions: {
-					compress: true,
-					mangle: true,
-					minify: true,
+					compress: prod,
+					mangle: prod,
+					minify: prod,
+					format: {
+						comments: prod ? false : 'all'
+					}
 				}
 			}
-		}
+		},
 	},
 };
 
 export default defineConfig({
 	source: {
-    entry: {
-      index: './src/index.ts',
-    },
+		entry: {
+			index: './src/index.ts',
+		},
 		transformImport: [
-      {
-        libraryName: 'lodash',
-        customName: 'lodash/{{ member }}',
-      },
-    ],
-  },
+			{
+				libraryName: 'lodash',
+				customName: 'lodash/{{ member }}',
+			},
+		],
+	},
 	lib: [
 		{
 			...sharedLib,
 			format: 'esm',
+			output: {
+				distPath: { root: `${basePath}/esm` },
+			}
 		},
 		{
 			...sharedLib,
 			format: 'cjs',
+			output: {
+				distPath: { root: `${basePath}/cjs` },
+			}
 		},
 	]
 });
